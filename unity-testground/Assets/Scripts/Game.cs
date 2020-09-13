6028d0bca5f98108106caa30b6cc67a23d43d1ab
Kevin <kevin.ka_96@hotmail.com>
2020-09-13 16:58:59 +0200

    Detecting keycodes
    A script for detecting whether or not a specific key press is detected

---
+++ unity-testground/Assets/Scripts/Game.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    //Array[] keys = new Array[10];
+    int _gameNumber = 1;
+    bool _gameReady;
+    //KeyCode[] keys;
+    private void Start()
+    {
+        _gameReady = true;
+        JoystickActivation();
+    }
+
+    void Update()
+    {
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        if (_gameReady)
+        {
+            for (int i = 0; i < JoystickActivation().Count; i++)
+            {
+                if (Input.GetKeyDown(JoystickActivation()[i]))
+                {
+                    Debug.Log("Keycode: " + JoystickActivation()[i]);
+                    _gameReady = false;
+                    break;
+                }
+            }
+        }
+    }
+
+    private List<KeyCode> JoystickActivation()
+    {
+        List<KeyCode> keys = new List<KeyCode>();
+
+        for (int i = 0; i < 20; i++)
+        {
+            KeyCode key = (KeyCode)Enum.Parse(typeof(KeyCode),"Joystick" + _gameNumber + "Button" + i);
+            keys.Add(key);
+        }
+        keys.Add(KeyCode.K); //For testing
+        return keys;
+    }
+}
