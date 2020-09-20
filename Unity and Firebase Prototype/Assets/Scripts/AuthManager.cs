4312794534e34ac1dc9f85ca44779d04c24208af
Kevin <kevin.ka_96@hotmail.com>
2020-09-20 17:24:23 +0200

    Firebase Unity Prototype
    Setting things up

---
+++ Unity and Firebase Prototype/Assets/Scripts/AuthManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using UnityEngine.UI;
+
+public class AuthManager : MonoBehaviour
+{
+    // Firebase
+
+    [Header("Firebase")]
+
+    public DependencyStatus dependencyStatus;
+    public FirebaseAuth auth;
+    public FirebaseUser user;
+
+    [Header("Login")]
+
+    public InputField emailLoginField;
+    public InputField passwordLoginField;
+    public Text warningLoginText;
+    public Text confirmLoginText;
+
+    [Header("Register")]
+
+    public InputField usernameRegField;
+    public InputField emailRegField;
+    public InputField passwordRegField;
+    public InputField passwordRegVerifyField;
+    public Text warningRegText;
+    
+}
---
+++ Unity and Firebase Prototype/Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // Color palette 
+    // 
+    // Yellow               FFCB2A
+    // Dark Orange          F5820C
+    // Light Orange         FFA610
+    // Background Blue      051E34
+    // Light Blue           1A73E8
+    //
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
